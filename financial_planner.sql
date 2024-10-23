79ac3399246684ef414a947692717aa2060e49af
ZackW6 <154006365+ZackW6@users.noreply.github.com>
2024-10-23 12:36:38 -0500

    sql file save

--- financial_planner.sql
+++ financial_planner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2024 at 04:50 AM
+-- Generation Time: Oct 23, 2024 at 07:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,7 +34,7 @@ CREATE TABLE `users` (
   `phone` varchar(20) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `jsonFile` mediumtext NOT NULL
+  `jsonFile` mediumtext NOT NULL DEFAULT '{   "email": "",   "expenses": [     {       "title": "firstExpense",       "percentage": "",       "amount": "10"     }   ] }'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,7 +42,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullName`, `email`, `phone`, `password`, `reg_date`, `jsonFile`) VALUES
-(1, 'Siddharth Pandit ', 'siddharth20pandit@gmail.com', '', '$2y$10$0OfBV6gvlyMMUgFt4MH0a.RytOXMt7tJN3nz.k4rv1nheLFeQVF8i', '2024-09-05 22:52:46', '');
+(1, 'Siddharth Pandit ', 'siddharth20pandit@gmail.com', '', '$2y$10$0OfBV6gvlyMMUgFt4MH0a.RytOXMt7tJN3nz.k4rv1nheLFeQVF8i', '2024-09-05 22:52:46', ''),
+(7, 'Zack', 'zackarchon1@gmail.com', '', '$2y$10$OzErIy1uxVZlqk5eSFAtJ.kSpL8e.mJWjXXY0l2wyjEqXg30AkA.a', '2024-10-23 05:16:15', '{\n  \"email\": \"zackarchon1@gmail.com\",\n  \"expenses\": [\n    {\n      \"title\": \"firstExpense\",\n      \"amount\": \"100\",\n      \"influence\": \"positive\",\n      \"dailyData\": [\n        {\n          \"date\": \"2024-10-12\",\n          \"amount\": \"10\"\n        },\n        {\n          \"date\": \"2024-10-28\",\n          \"amount\": \"500\"\n        },\n        {\n          \"date\": \"2024-10-09\",\n          \"amount\": \"100\"\n        },\n        {\n          \"date\": \"2024-10-23\",\n          \"amount\": \"120\"\n        },\n        {\n          \"date\": \"2024-09-12\",\n          \"amount\": \"70\"\n        },\n        {\n          \"date\": \"2024-10-24\",\n          \"amount\": \"199\"\n        }\n      ]\n    },\n    {\n      \"title\": \"otherExpense\",\n      \"amount\": \"12\",\n      \"influence\": \"positive\",\n      \"dailyData\": [\n        {\n          \"date\": \"2024-09-30\",\n          \"amount\": \"100\"\n        }\n      ]\n    }\n  ]\n}');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
